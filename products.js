56289cc7aaf47f14a7fe8311bd1694ab6e63e4fa
Jamal Javadov <camalcavadovwork@gmail.com>
2025-06-27 14:39:25 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,125 @@
+// products.js
+
+const products = [
+  {
+    images: ["images/a1.webp", "images/a2.webp", "images/a3.webp"],
+    price: "13.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["exorcism", "sep", "xaç"]
+  },
+  {
+    images: ["images/f1.webp", "images/f2.webp", "images/f3.webp"],
+    price: "13.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["qədim", "sep", "naxışlı"]
+  },
+  {
+    images: ["images/v1.webp", "images/v2.webp", "images/v3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "naxışlı"]
+  },
+  {
+    images: ["images/m1.webp", "images/m2.webp", "images/m3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "masson"]
+  },
+  {
+    images: ["images/mm1.webp", "images/mm2.webp", "images/mm3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "maska","13"]
+  },
+  {
+    images: ["images/s1.webp", "images/s2.webp", "images/s3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "skeleton"]
+  },
+  {
+    images: ["images/q1.webp", "images/q2.webp", "images/q3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük"]
+  },
+    {
+    images: ["images/nd1.webp", "images/nd2.webp", "images/nd3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "qədim","viking","naxışlı"]
+  },
+      {
+    images: ["images/bat1.webp", "images/bat2.webp", "images/bat3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "heyvan"]
+  },
+        {
+    images: ["images/sk1.webp", "images/sk2.webp", "images/sk3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "skeleton"]
+  },
+    {
+    images: ["images/b1.webp", "images/b2.webp", "images/b3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük"]
+  },
+  {
+    images: ["images/g1.webp", "images/g2.webp", "images/g3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "naxışlı"]
+  },
+{
+    images: ["images/ff1.webp", "images/ff2.webp", "images/ff3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "heyvan"]
+  },
+{
+    images: ["images/i1.webp", "images/i2.webp", "images/i3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "heyvan"]
+  },
+  {
+    images: ["images/p1.webp", "images/p2.webp", "images/p3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "naxışlı"]
+  },
+    {
+    images: ["images/d1.webp", "images/d2.webp", "images/d3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük"]
+  },
+    {
+    images: ["images/h1.webp", "images/h2.webp", "images/h3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "xaç"]
+  },
+    {
+    images: ["images/hs1.webp", "images/hs2.webp", "images/hs3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "skeleton"]
+  },
+  {
+    images: ["images/thor1.webp", "images/thor2.webp", "images/thor3.webp"],
+    price: "12.99 AZN",
+    material: "Paslanmaz Polad",
+    tags: ["üzük", "qədim"]
+  },
+
+];
+
+// İnstagram səhifənizin linkini bura daxil edin
+const instagramLink = "https://www.instagram.com/ringforbaku?igsh=Nnc1ZHYzamczMzM3&utm_source=qr"; // USERNAME yerinə öz istifadəçi adınızı yazın
+// products.js (faylın sonuna əlavə edin)
+
+const whatsappLink = "http://wa.me/994518320315"; // DİQQƏT: 994XXYYYYYYY yerinə öz nömrənizi beynəlxalq formatda (+ işarəsi olmadan) yazın. Məsələn: 994501234567
